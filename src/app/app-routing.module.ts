a455839049439b58c72dcfbf6e48254dbfb67108
Joao Victor <victorsrodrigues99@gmail.com>
2024-05-16 19:42:58 -0300

    Adicionando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'criarPensamento',
+    component: CriarPensamentosComponent
+  },
+
+  {
+    path: 'listarPensamento',
+    component: ListarPensamentosComponent
+  },
+
+  {
+    path: '',
+    redirectTo: 'listarPensamento',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
