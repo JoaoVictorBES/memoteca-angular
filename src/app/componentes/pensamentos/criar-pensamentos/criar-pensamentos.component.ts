797b3fafc3a254d7f2e9b1e4175be9c391a670eb
Joao Victor <victorsrodrigues99@gmail.com>
2024-05-15 21:41:18 -0300

    Adicionando data binding two-way

--- src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -13,5 +13,12 @@ export class CriarPensamentosComponent {
     autoria: 'Dev',
     modelo: ''
   }
-  
+
+  criarPensamento () {
+    alert("Novo pensamento criado")
+  }
+
+  cancelarPensamento() {
+    alert ("Pensamento cancelado")
+  }
 }
